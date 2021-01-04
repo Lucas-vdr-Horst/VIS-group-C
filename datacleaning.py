b100c6052d2f8d95d09564e9cda9d07b850de26b
Max2411 <maxvankemenade@gmail.com>
2021-01-04 15:43:55 +0100

    updates cleaning into a function

--- datacleaning.py
+++ datacleaning.py
@@ -1,7 +1,7 @@
 def fix_hashtags(filename):
-    """ splits the hashtag that are attached to the datetime with ';', the seperator of the csv file"""
     new_filename = f"..\VIS-group-C\dataset\sensor_data\{'new_'+filename}"
     filename = f"..\VIS-group-C\dataset\sensor_data\{filename}"
+
     lines = []
     with open(filename,"r") as file:
         for line in file.readlines():
