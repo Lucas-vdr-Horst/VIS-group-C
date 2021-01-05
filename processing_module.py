6600cc89528699b31ec7ef817ebd7f08f3247817
AmaryllisLee <ama.lee@student.hu.nl>
2021-01-05 15:50:08 +0100

    aanpassing process function

--- processing_module.py
+++ processing_module.py
@@ -15,10 +15,14 @@ def process(begin_time, end_time):
     if not Path("./dataset/sensor_data/new_BOS210.csv").is_file():
         fix_hashtags("BOS210.csv")
     df_sensor = pd.read_csv("./dataset/sensor_data/new_BOS210.csv", delimiter=";", low_memory=False)
-    data = df_sensor.iloc[begin_time:end_time]
+    df_sensor = df_sensor.set_index('time') # set index to time
+    data = df_sensor.loc[begin_time:end_time] # filter to begin and end time
+
 
 if __name__ == "__main__":
-    process(554, 556)
+    process("02-11-2020 00:00:00.0","02-11-2020 00:00:00.6")
+
+
 
 # [     long       lat         long     lat       long      lat
 #   [[378328794, 4893289004], [893924, 483724], [48328094, 38492043]],  auto1
