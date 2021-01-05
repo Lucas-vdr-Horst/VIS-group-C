0fb32cf0f70b47944852a4cadef92d2079eb3848
freekgj <freekgj@hotmail.com>
2021-01-05 09:41:00 +0100

    fix docstring

--- processing_module.py
+++ processing_module.py
@@ -4,16 +4,19 @@ from data_cleaning import fix_hashtags
 
 def process(begin_time, end_time):
     """
-    :Input begintime als 554. Dit betekend 00 uur 00 min 55 sec .4 milisec
+    Geeft json terug iets zoals hier beneden
+
+    :param begin_time: als 554. Dit betekend 00 uur 00 min 55 sec .4 milisec
     :type str
-    Input endtime als 554. Dit betekend 00 uur 00 min 55 sec .4 milisec
+
+    :param end_time: als 554. Dit betekend 00 uur 00 min 55 sec .4 milisec
     :type str"""
-    """Geeft json terug iets zoals hier beneden"""
+
     if not Path("./dataset/sensor_data/new_BOS210.csv").is_file():
-        print("hi")
         fix_hashtags("BOS210.csv")
     df_sensor = pd.read_csv("./dataset/sensor_data/new_BOS210.csv", delimiter=";", low_memory=False)
     data = df_sensor.iloc[begin_time:end_time]
+
 
 process(554, 556)
 
