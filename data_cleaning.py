c39f47f57471ec9190c18a83790591b843ed0e41
Max2411 <maxvankemenade@gmail.com>
2021-01-06 16:19:52 +0100

    Implemented get_available_times

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,12 +1,13 @@
 import os
+import shutil
 
 
-def fix_hashtags(filename):
+def fix_hashtags(intersection_name, filename):
     """ splits the hashtag that are attached to the datetime with ';', the seperator of the csv file"""
-    new_filename = os.path.join("dataset", "sensor_data",f"new_{filename}")
-    filename = os.path.join("dataset", "sensor_data", filename)
+    new_filename = os.path.join("dataset", intersection_name, f"new_{filename}")
+    old_filename = os.path.join("dataset", intersection_name, filename)
     lines = []
-    with open(filename,"r") as file:
+    with open(old_filename, "r") as file:
         for line in file.readlines():
             if line[21] == "#":
                 line = line.replace("#", ";#", 1)
@@ -15,7 +16,8 @@ def fix_hashtags(filename):
     with open(new_filename, "w") as f:
         for line in lines:
             f.write(line)
+    shutil.move(old_filename, os.path.join("dataset", "Original", filename))
 
 
 if __name__ == "__main__":
-    fix_hashtags("BOS210.csv")
+    fix_hashtags("BOS210", "BOS210.csv")
