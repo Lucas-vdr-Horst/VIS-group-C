8c631a4dea5db04450ba0c3d41473d89c63fad32
freekgj <freekgj@hotmail.com>
2021-01-08 16:25:17 +0100

    fixing docstrings

--- processing_module.py
+++ processing_module.py
@@ -83,7 +83,15 @@ def load_sensor_data(begin_time, end_time):
 
 
 def get_coordinates_lane(genericlane):
-    """Haal alle longitude en latitude uit nodes van gegeven laneID"""
+    """
+    Extract all longitude en latitude of a specific lane
+    
+    :param genericlane: a variable what contains 1 lane
+    :type xml
+
+    :returns coordinates: all coordinates of a lane
+    :type list
+    """
     coordinaten = []
     for node in genericlane.iter('nodes'):  # i.iter('nodes') == nodes
         for n in node.iter('node-LatLon'):  # node.iter('node-LatLon') == node-latlon
@@ -94,6 +102,15 @@ def get_coordinates_lane(genericlane):
 
 
 def get_all_lanes_coordinates(tree):
+    """
+    Returns a Json file with the coordinates of all lanes.
+
+    :param tree: info about the intersections
+    :type xml
+
+    :returns: file with all coordinates from all lanes
+    :type json
+    """
     paden_auto = pd.DataFrame(columns = ['Rijbaan', 'ingress_coordinaten', 'trajectory_coordinaten', 'egress_coordinaten'])
     root = tree.getroot()
     dict_with_coords = {}
@@ -130,14 +147,14 @@ def get_all_lanes_coordinates(tree):
 
 def process(begin_time, end_time):
     """
-    Geeft json terug iets zoals hier beneden
+    Returns a Json file.
 
-    :param begin_time: als 554. Dit betekend 00 uur 00 min 55 sec .4 milisec
-    :type str
+    :param begin_time: Datetime dd-mm-yyyy hh:mm:ss:m starttime
+    :type Datetime
 
-    :param end_time: als 554. Dit betekend 00 uur 00 min 55 sec .4 milisec
-    :type str"""
-
+    :param end_time: Datetime dd-mm-yyyy hh:mm:ss:m endtime
+    :type Datetime
+    """
 
     tree = ET.parse('intersections/BOS210/79190154_BOS210_ITF_COMPLETE.xml')
     df_paden = get_all_lanes_coordinates(tree)
