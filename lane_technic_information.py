92b62bc24638c0d1f478db30e03dc128f9e4d3cd
freekgj <freekgj@hotmail.com>
2021-01-11 17:34:42 +0100

    Bug fix: all lanes stored in dict works only for BOS210 at the moment!

--- lane_technic_information.py
+++ lane_technic_information.py
@@ -11,7 +11,13 @@ def get_signalGroup_from_lane(laneID, root):
     laneSet = root[2][1][0][6] # topology/mapData/intersections/laneset
     for genericlane in laneSet:
         if genericlane[0].text == laneID:
-            return genericlane[5][0][2].text
+            try:
+                signalgroup = genericlane[5][0][1].text
+
+            except:
+                signalgroup = None
+
+            return signalgroup
 
 
 def get_trafficsignal_name(signalgroupnumber, root):
@@ -19,16 +25,16 @@ def get_trafficsignal_name(signalgroupnumber, root):
     for signalgroup in signalgroups:
         if signalgroup[0].text == signalgroupnumber:
             return signalgroup[0].text
-            
 
 
 def get_dict_lane_info():
     """
-    Returns a dict with the induction loops and trafficlights of a lane. Keys are Str
+    Makes a dict with the induction loops and trafficlights of a lane. Keys are presented in string.
 
-    :returns: file with all coordinates from all lanes
-    :type csv
+    :returns: all technical info of a lane
+    :type dict
     """
+
     xml_file = open_xml()
     dict_info_lanes = {}
     sensors = xml_file[3][0][7][0][4][0][5] #topology/controlData/controller/controlUnits/controlUnit/controlledIntersections/controlledIntersection/sensors
@@ -36,18 +42,19 @@ def get_dict_lane_info():
         try:
             laneID = sensor[8][0][0].text
         except:
-            break
+            continue
 
         if laneID not in dict_info_lanes:
             dict_info_lanes[laneID] = {'induction_loops': [], 'traffic_light': ''}
         sensor_name = (sensor[1].text)
         dict_info_lanes[laneID]['induction_loops'].append(sensor_name)
         signal_group = get_signalGroup_from_lane(laneID, xml_file)
-        traffic_light = get_trafficsignal_name(signal_group, xml_file)
-        dict_info_lanes[laneID]['traffic_light'] = traffic_light
+        if signal_group != None:
+            traffic_light = get_trafficsignal_name(signal_group, xml_file)
+            dict_info_lanes[laneID]['traffic_light'] = traffic_light
     return dict_info_lanes
 
 
 if __name__ == "__main__":
     dict_with_info = get_dict_lane_info()
-    print(dict_with_info['1'])
+    print(dict_with_info)
