3960b2a5b6b75ed66f067d07846ab15b4ae41faa
RichardDev01 <60653249+RichardDev01@users.noreply.github.com>
2021-01-13 14:54:06 +0100

    added car init

--- simulation/classes/car.py
+++ simulation/classes/car.py
@@ -1,3 +1,5 @@
+import location
+
 class car:
     """
     This class keeps track of properties of a car.
@@ -11,5 +13,19 @@ class car:
     + method(type): type
     """
 
-    def __init__(self, id: int):
+    def __init__(self, id: int, length: int, speed:int, start_posistion, destination):
         self.id = id
+        self.length = length
+        self.speed = speed
+        self.start_position = start_posistion
+        self.destination = destination
+        self.location = location() # Lane toevoegen
+
+    def move(self):
+        """
+        The move function is responsible for checking if the car can move forward and updates it's own posistion
+        @rtype: object
+        """
+        # TODO make this function correct
+        self.location()
+
