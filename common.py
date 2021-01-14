b567d139ad70995595fbe06808d22ae7b9879aa7
freekgj <freekgj@hotmail.com>
2021-01-14 15:30:04 +0100

    function for exact coordinates of a point between two coordinates

--- common.py
+++ common.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import xml.etree.ElementTree as ET
 from datetime import datetime
 from const import intersection_data_location
 
@@ -46,3 +47,19 @@ def get_header(csv_filepath: str) -> str:
 def datetime_string_to_milli(datetime_string) -> int:
     """Datetime in string format '%d-%m-%Y %H:%M:%S.%f' to milliseconds since 1970"""
     return int(datetime.strptime(datetime_string, '%d-%m-%Y %H:%M:%S.%f').timestamp()*1000)
+
+
+def open_xml(file_name):
+    """
+    Returns a xml out of the given directory.
+    
+    :param file_name: the name of the xml put in like this -> 'BOS210/79190154_BOS210_ITF_COMPLETE.xml'
+    :type str
+
+    :returns: an xml readable in python
+    :type xml.Element
+    """
+    os.chdir("..")
+    tree = ET.parse(get_xml_path(file_name)[0])
+    root = tree.getroot()
+    return root
--- simulation/length_per_laneID.py
+++ simulation/length_per_laneID.py
@@ -1,28 +1,12 @@
 import xml.etree.ElementTree as ET
 import os
 import sys
-from common import get_xml_path
+from common import open_xml
 import collections
 
 sys.path.append('../')
 
 from const import intersection_data_location
-
-
-def open_xml(file_name):
-    """
-    Returns a xml out of the given directory.
-    
-    :param file_name: the name of the xml put in like this -> 'BOS210/79190154_BOS210_ITF_COMPLETE.xml'
-    :type str
-
-    :returns: an xml readable in python
-    :type xml.Element
-    """
-    os.chdir("..")
-    tree = ET.parse(get_xml_path(file_name)[0])
-    root = tree.getroot()
-    return root
 
 
 def get_length_per_lane(single_lane):
