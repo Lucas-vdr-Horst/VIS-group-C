fa424f6913a346a283e5dd752cf8a1c29aa5f823
freekgj <freekgj@hotmail.com>
2021-01-18 14:05:57 +0100

    Get distance from stoplane to the start and end of a sensor

--- simulation/classes/InductionCoil.py
+++ simulation/classes/InductionCoil.py
@@ -37,3 +37,38 @@ class InductionCoil:
     
     def getState(self):
         return self.state
+
+    def calculate_sensor_point(self):
+        lane_id = Location.self.lane_id
+        
+        length_to_sides = self.lengte / 2
+
+        begin_node = Location.Lane.self.nodes[0]
+
+        distance_between_coordinates = np.sqrt((self.centerLocation[0]-begin_node[0])**2 + (self.centerLocation[1]-begin_node[1]))
+        induction_start_point_meters = distance_between_coordinates - length_to_sides
+        induction_end_point_meters = distance_between_coordinates + length_to_sides
+        self.induction_start_point_meters = 
+        
+        return [induction_start_point_meters, induction_end_point_meters]
+    
+    
+    def convert_distance_to_coordinates(self):
+        begin_node = Location.Lane.self.nodes[-1]
+        end_node = Location.Lane.self.nodes[0]
+
+        distance_between_coordinates = np.sqrt((coordinate2[0] - coordinate1[0]) ** 2 + (coordinate2[1] - coordinate1[1]) ** 2)
+    
+        start_node_sensor, end_node_sensor = InductionCoil.calculate_sensor_point
+    
+        distance_ratio = start_node_sensor / distance_between_coordinates
+        lat = coordinate1[0] + distance_ratio * (coordinate2[0] - coordinate1[0])
+        lon = coordinate1[1] + distance_ratio * (coordinate2[1] - coordinate1[1])
+
+        self.sensor_start_coordinates = [lat, lon]
+        
+        distance_ratio = end_node_sensor / distance_between_coordinates
+        lat = coordinate1[0] + distance_ratio * (coordinate2[0] - coordinate1[0])
+        lon = coordinate1[1] + distance_ratio * (coordinate2[1] - coordinate1[1])
+
+        self.sensor_end_coordinates - [lat, lon]
