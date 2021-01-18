fa40a10931bb160c8eee883d285a8293d8c19a75
freekgj <freekgj@hotmail.com>
2021-01-18 14:06:16 +0100

    Read all XML files instead of 1

--- simulation/length_per_laneID.py
+++ simulation/length_per_laneID.py
@@ -1,13 +1,13 @@
 import xml.etree.ElementTree as ET
 import os
 import sys
-from common import open_xml
 import collections
 
-sys.path.append('../')
+sys.path.append('./')
+from common import open_xml
+
 from const import intersection_data_location
 from preprocess.processing_module import get_lane
-
 
 
 def get_length_per_lane(single_lane):
@@ -35,22 +35,28 @@ def get_length_all_lanes(intersection_name):
     :returns: an dictionary with all length of a lane by laneID
     :type dict
     """
-    xml_file = open_xml(intersection_name)
 
     dict_lanes = {}
-    for approachess in xml_file[3][0][7][0][4][0][4]:
-        for approach in approachess:
-            for approach_lane in approach:
 
-                info_lane = get_length_per_lane(approach_lane)
-                lane_id = info_lane[0]
-                lane = get_lane(xml_file, lane_id)
+    for intersection_name in os.listdir('./intersections'):        
+        path = os.path.join('intersections', intersection_name)
+        if os.path.isdir(path):
+            for xml_file in os.listdir(path):
+                if xml_file.endswith('.xml'):
+                    xml_file = open_xml(intersection_name)
+                    for approachess in xml_file[3][0][7][0][4][0][4]:
+                        for approach in approachess:
+                            for approach_lane in approach:
+                            
+                                info_lane = get_length_per_lane(approach_lane)
+                                lane_id = info_lane[0]
+                                lane = get_lane(xml_file, lane_id)
 
-                if lane[3][2].tag == 'vehicle' :
-                    #Check whether lane is ingress, egress or an trajectory with element directionaluse (genericlane[3][2])
-                    #Content of directionaluse give a Bit 10( ingresspath)or 01 (egresspath)
-                    #AMa's comment: meer duidelijk dan ingressAproah of egressAproach vind ik , wil delete later
-                    dict_lanes[lane_id] = {'lane':lane, 'length':info_lane[1]}
+                                #if lane[3][2].tag == 'vehicle' :
+                                    #Check whether lane is ingress, egress or an trajectory with element directionaluse (genericlane[3][2])
+                                    #Content of directionaluse give a Bit 10( ingresspath)or 01 (egresspath)
+                                    #AMa's comment: meer duidelijk dan ingressAproah of egressAproach vind ik , wil delete later
+                                dict_lanes[lane_id] = {'lane':lane_id, 'length':info_lane[1]}
     return dict_lanes
 
 
