6303571c02b5ac9cff535532d83f7cc76f2c71c1
freekgj <freekgj@hotmail.com>
2021-01-21 14:35:27 +0100

    Added docstrings and defined return value of lane_technical_information

--- preprocess/lane_technical_information.py
+++ preprocess/lane_technical_information.py
@@ -6,7 +6,14 @@ sys.path.append('./')
 from common import open_xml
 
 
-def get_signalGroup_from_lane(laneID, root):
+def get_signalGroup_from_lane(laneID, root) -> str:
+    """
+    Get the signalgroup name connected to a lane.
+
+    @params laneID: laneID of the specific lane
+    @params root: the xml file of an intersection
+    @returns: a string with the signalgroupname of a specific lane
+    """
     laneSet = root[2][1][0][6] # topology/mapData/intersections/laneset
     for genericlane in laneSet:
         if genericlane[0].text == laneID:
@@ -19,22 +26,26 @@ def get_signalGroup_from_lane(laneID, root):
             return signalgroup
 
 
-def get_trafficsignal_name(signalgroupnumber, root):
+def get_trafficsignal_name(signalgroupnumber, root) -> str:
+    """
+    Get the trafficlight name that correspons to a signalgroupnumber
+
+    @params signalgroupnumber: an integer number that represents the signalgroupnumber
+    @params root: the xml file of an intersection
+    @returns: a string with the name of a trafficlight
+    """
     signalgroups = root[3][0][7][0][4][0][6]  # topology/controlData/controller/controlUnits/controlUnit/controlledIntersections/controlledIntersection/sensors
     for signalgroup in signalgroups:
         if signalgroup[1].text == str(signalgroupnumber):
             return signalgroup[0].text
 
 
-def get_dict_lane_info(file_name):
+def get_dict_lane_info(file_name)-> dict:
     """
     Makes a dict with the induction loops and trafficlights of a lane. Keys are presented in string.
-    
-    :param file_name: the locationname of the CSV put in like this -> 'BOS210/79190154_BOS210_ITF_COMPLETE.xml'
-    :type str
 
-    :returns: all technical info of a lane
-    :type dict
+    @param file_name: the directoryname where the xml are put in
+    @returns: all technical info of a lane in a dictionary
     """
 
     xml_file = open_xml(file_name)
