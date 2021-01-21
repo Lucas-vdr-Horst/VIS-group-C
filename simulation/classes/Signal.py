8da70ccee9c6323899a53b6ee5af3454498e4e88
RichardDev01 <60653249+RichardDev01@users.noreply.github.com>
2021-01-21 14:25:20 +0100

    forgot to push with branhce

--- simulation/classes/Signal.py
+++ simulation/classes/Signal.py
@@ -6,13 +6,20 @@ class Signal:
     """
 
 
-    def __init__(self, id: int, signalManager : SignalManager = None):
+    def __init__(self, id: int, signalManager : SignalManager = None, intersection : str = None):
         self.id = id        # id of the signal/ traficlight
         self.signalManager = None
+        self.intersection = None
     
 
     def setSignalManager(self, signalMan : SignalManager):
         self.signalManager = signalMan
+    
+    def setIntersection(self, filename : str):
+        self.intersection = filename
+    
+    def getIntersection(self):
+        return self.intersection
 
     def getState(self, time) -> str:
-        return self.signalManager.getState(self.id, time) 
+        return self.signalManager.getState(self.id, time, self.intersection) 
