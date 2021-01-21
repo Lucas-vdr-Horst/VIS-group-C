5fa95b5b4a012d93f01f683e30f823d06731bc5e
freekgj <freekgj@hotmail.com>
2021-01-21 16:44:00 +0100

    Test process spawnpoints

---
+++ tests/test_process_spawnpoints.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+import os
+import pandas as pd
+
+import sys
+
+from preprocess.process_spawnpoints import process_certain_positions
+
+sys.path.append('./')
+
+from simulation.run_simulation import load_lanes_signals_and_inductioncoils
+
+
+class Test(TestCase):
+
+    def test_process_certain_positions(self):
+
+        dict_induction_coils = load_lanes_signals_and_inductioncoils()
+        process_certain_positions(dict_induction_coils)
+
+        path = os.path.join("tests", "filetest", "test_spawn_points.csv")
+        expected_output = pd.read_csv(path, sep=';', nrows=9)
+
+        path2 = os.path.join("preprocess", "output", "spawn_points.csv")
+        output_file = pd.read_csv(path2, sep=';', nrows=9)
+        
+        output = pd.DataFrame.equals(output_file, expected_output)
+
+        self.assertEqual(output, True)
